ac2d6661c3743c58dfc0fe9f7b5940a4b679fd96
314159otr <piotrbpersonal@gmail.com>
2026-06-08 15:38:15 +0200

    CH1: Servers - L5: Fileservers

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import(
 func main() {
 	const port = "8080"
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := &http.Server{
 		Addr: ":" + port,
 		Handler: serveMux,
