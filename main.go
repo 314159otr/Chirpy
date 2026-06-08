7159a2a544062c8fae7abd267399134535d65f25
314159otr <piotrbpersonal@gmail.com>
2026-06-08 15:30:12 +0200

    CH1: Servers - L4: Server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+	"net/http"
+	"log"
+)
+
+func main() {
+	const port = "8080"
+	serveMux := http.NewServeMux()
+	server := &http.Server{
+		Addr: ":" + port,
+		Handler: serveMux,
+	}
+	log.Printf("Starting the server on port: %s\n", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
