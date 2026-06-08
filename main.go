34a83b5a54fea2c428f59bd10e0324b8402a421b
314159otr <piotrbpersonal@gmail.com>
2026-06-08 19:25:17 +0200

    CH2: Routing - L2: Stateful Handlers

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import(
 	"net/http"
 	"log"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("/healthz", handlerReadiness)
+	serveMux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	serveMux.HandleFunc("/reset", apiCfg.handlerReset)
 
 
 	server := &http.Server{
@@ -27,4 +39,24 @@ func handlerReadiness(w http.ResponseWriter, req * http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req * http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, req * http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reseted to 0"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
